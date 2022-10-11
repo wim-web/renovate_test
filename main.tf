2795dbe9f8aee45d66a223e0a2242a58a744027f
wim-web <45655999+wim-web@users.noreply.github.com>
2022-10-11 14:57:47 +0900

    iiii

--- main.tf
+++ main.tf
@@ -3,3 +3,22 @@ module "log_monitors" {
   version = "0.2.0"
 
 }
+
+module "log_alerts" {
+  version = "0.5.0"
+  source  = "app.terraform.io/torana/alert/datadog//modules/log"
+}
+
+module "pien" {
+  version = "1.1.1"
+  name    = "name"
+  source  = "app.terraform.io/torana/ecs/aws"
+}
+
+
+module "pien2" {
+  source = "app.terraform.io/torana/ecs/aws"
+
+  name    = "name2"
+  version = "1.5.1"
+}
